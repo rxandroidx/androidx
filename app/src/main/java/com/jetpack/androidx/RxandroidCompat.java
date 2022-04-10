a61fb25260efa2f35fcfe9f026863c8d8db29432
SuryaBhati <suryabhati203@gmail.com>
2022-04-10 18:14:35 +0530

    3nd Release

---
+++ app/src/main/java/com/jetpack/androidx/RxandroidCompat.java
@@ -0,0 +1,16 @@
+package com.jetpack.androidx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RxandroidCompat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rxandroid_compat);
+
+
+    }
+}
